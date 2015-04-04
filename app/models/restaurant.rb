45e708afb73fbaaca86786e4f79d96388744a574
bstil22 <bradydynamite@hotmail.com>
2015-04-03 22:30:56 -0700

    changed model back to working version

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
-  searchkick callbacks: :async
+  searchkick word_start: [:name]
 end
