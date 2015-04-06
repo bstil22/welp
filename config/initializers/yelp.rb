675ae7c95c818aa5d8ede0d01656baa69f362be0
bstil22 <bradydynamite@hotmail.com>
2015-04-05 20:33:29 -0700

    third time's a charm...

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
 require 'yelp'
 
 Yelp.client.configure do |config|
-  config.consumer_key = ENV["YELP_CONSUMER_KEY"]'vUk27oLj0COZ0Dygt2MIKQ'
-  config.consumer_secret = ENV["YELP_CONSUMER_SECRET"]'q4xa7ShahaHz5zSuRke2I7sPUKM'
-  config.token = ENV["YELP_TOKEN"]'LuoOC3DfYMWd-bUJA1GBoTuW_BWm1X0L'
-  config.token_secret = ENV["YELP_SECRET_TOKEN"]'RlszmCCvxb8RTa15xfAvH7VhiQE'
+  config.consumer_key = ENV["YELP_CONSUMER_KEY"]
+  config.consumer_secret = ENV["YELP_CONSUMER_SECRET"]
+  config.token = ENV["YELP_TOKEN"]
+  config.token_secret = ENV["YELP_SECRET_TOKEN"]
 end
