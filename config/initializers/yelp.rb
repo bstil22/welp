b3d746336beb246e35abae0516e28edbe521803c
bstil22 <bradydynamite@hotmail.com>
2015-04-05 20:27:39 -0700

    made yelp private..

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
 require 'yelp'
 
 Yelp.client.configure do |config|
-  config.consumer_key = 'vUk27oLj0COZ0Dygt2MIKQ'
-  config.consumer_secret = 'q4xa7ShahaHz5zSuRke2I7sPUKM'
-  config.token = 'LuoOC3DfYMWd-bUJA1GBoTuW_BWm1X0L'
-  config.token_secret = 'RlszmCCvxb8RTa15xfAvH7VhiQE'
+  config.consumer_key = ENV["YELP_CONSUMER_KEY"]'vUk27oLj0COZ0Dygt2MIKQ'
+  config.consumer_secret = ENV["YELP_CONSUMER_SECRET"]'q4xa7ShahaHz5zSuRke2I7sPUKM'
+  config.token = ENV["YELP_TOKEN"]'LuoOC3DfYMWd-bUJA1GBoTuW_BWm1X0L'
+  config.token_secret = ENV["YELP_SECRET_TOKEN"]'RlszmCCvxb8RTa15xfAvH7VhiQE'
 end
